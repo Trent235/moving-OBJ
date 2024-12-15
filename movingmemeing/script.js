64620c5b3c15a915865318b4cb22642781c49d7a
Trinity Ison <tison90802@uat.edu>
2024-12-15 14:53:21 -0600

    Add files via upload

---
+++ movingmemeing/script.js
@@ -0,0 +1,22 @@
+let interval;             //defining the veariable  
+const image = document.getElementById('memeImage');     //image 
+
+function startMoving(direction) {       //call fuction
+    clearInterval(interval); // Clear any existing interval
+    interval = setInterval(() => {
+        let left = parseInt(window.getComputedStyle(image).left, 10);
+        let top = parseInt(window.getComputedStyle(image).top, 10);
+
+        if (direction === 'right') left += 5;   //parameter conditions          
+        if (direction === 'left') left -= 5;    //parameter conditions
+        if (direction === 'up') top -= 5;       //parameter conditions
+        if (direction === 'down') top += 5;     //parameter conditions
+
+        image.style.left = `${left}px`;         //css referene property
+        image.style.top = `${top}px`;           //css referene property
+    }, 50);
+}
+
+function stopMoving() {                     //fuction stop
+    clearInterval(interval);                   //clear intervil
+}
